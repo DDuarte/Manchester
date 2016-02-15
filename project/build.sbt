3bcd4a9957e14cd73902a137d659518b75b1df9a
DDuarte <dnpd.dd@gmail.com>
2016-02-15 15:46:37 +0000

    Add base SBT project
    Sample code by @hugoferreira (1/10/2015)

---
+++ project/build.sbt
@@ -0,0 +1,6 @@
+name := "Manchester"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+    
---
+++ project/src/main/scala/Boot.scala
@@ -0,0 +1,91 @@
+import scala.collection.SortedMap
+import scala.collection.immutable.Queue
+import scala.util.Random
+
+case class Persona(affinities: Map[String, Double] = Map())
+case class User(id: String, p: Persona) {
+  def init(): Unit = {
+
+  }
+}
+
+case class Page(id: String, links: List[Page] = List(), events: Map[Event, Double] = Map())
+
+class Event(val id: Int, f: () => List[(Int, Event)]) {
+
+}
+
+abstract class Simulation {
+  type Action = () => Unit
+  type Agenda = SortedMap[Long, Queue[Action]]
+  type Trigger = (() => Boolean, () => Unit)
+
+  protected var curtime = 0l
+  def currentTime: Long = curtime
+
+  private var agenda: Agenda = SortedMap()
+  private var triggers: List[Trigger] = List()
+
+  def schedule(delay: Int = 0)(block: => Unit) {
+    val time = currentTime + delay
+    agenda += (time -> agenda.getOrElse(time, Queue()).enqueue(() => block))
+  }
+
+  def schedule(condition: => Boolean)(action: => Unit) {
+    triggers ::= (() => condition, () => action)
+  }
+
+  protected def step() {
+    if (hasNext) {
+      curtime = agenda.head._1
+
+      while (agenda.contains(curtime)) {
+        println("*** time = " + currentTime + " ***")
+        val actions = agenda(curtime)
+        agenda -= curtime
+        processActions(actions)
+        processTriggers()
+      }
+    }
+  }
+
+  protected def processActions(actions: Seq[Action]) { actions.foreach(_()) }
+  protected def processTriggers() { triggers.filter(_._1()).foreach(_._2()) }
+  protected def hasNext = agenda.nonEmpty
+
+  def run() {
+    while (agenda.nonEmpty) step()
+  }
+}
+
+trait SimulationStatistics extends Simulation {
+  private var numActions: Int = 0
+
+  override def processActions(actions: Seq[Action]) {
+    numActions += actions.size
+    super.processActions(actions)
+  }
+
+  def actionCount = numActions
+}
+
+object Main extends App {
+  new Simulation {
+    val electronics = Page("electronics")
+    val lingerie = Page("lingerie")
+    val homePage = Page("homepage", List(electronics, lingerie))
+
+    def userInjector() {
+      if (currentTime < 100) {
+        schedule(1) {
+          println("New users: " + Random.nextInt(5))
+          userInjector()
+        }
+      }
+    }
+
+    schedule(0) { userInjector() }
+
+    run()
+  }
+}
