f32a6d635c438d3381e399362936421940291516
DDuarte <dnpd.dd@gmail.com>
2016-04-15 19:57:48 +0100

    frontend: load highcharts js in the pages that need them only

---
+++ frontend/public/js/simulation.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+
+    var options = {
+        chart: {
+            renderTo: 'visitsPerCategoryContainer',
+            type: 'column'
+        },
+        series: [{
+            name: 'Categories',
+            colorByPoint: true,
+            data: []
+        }],
+        drilldown: {
+            series: [{}]
+        },
+        legend: {
+            enabled: true
+        },
+        yAxis: {
+            title: {
+                text: 'Visits'
+            }
+
+        },
+        xAxis: {
+            type: 'category'
+        },
+        title: {
+            text: ''
+        },
+        subtitle: {
+            text: 'Click the columns to view sub-categories.'
+        },
+        plotOptions: {
+            series: {
+                borderWidth: 0,
+                dataLabels: {
+                    enabled: true,
+                    format: '{point.y}'
+                }
+            }
+        }
+    };
+
+    $.getJSON(window.location.pathname + '/visitsPerCategory', function(data) {
+        options.series[0].data = data.series;
+        options.drilldown.series = data.drilldown;
+        var chart = new Highcharts.Chart(options);
+    });
+
+});
