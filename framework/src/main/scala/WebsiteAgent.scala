4eac110bcbab33440f8b09cc8c999cdb6e11d64b
DDuarte <dnpd.dd@gmail.com>
2016-05-31 04:32:04 +0100

    More dummy website agents

--- framework/src/main/scala/WebsiteAgent.scala
+++ framework/src/main/scala/WebsiteAgent.scala
@@ -1,8 +1,6 @@
 trait WebsiteAgent {
   def id: String
-
   def modifyPage(page: Page, visitor: User): Page
-
   def notifyUserAction(user: User, currentPage: Option[Page], action: Action)
 }
 
@@ -10,4 +8,16 @@ case class DummyWebsiteAgent() extends WebsiteAgent {
   val id = "dummy"
   def modifyPage(page: Page, visitor: User): Page = page
   def notifyUserAction(user: User, currentPage: Option[Page], action: Action) {}
+}
+
+case class CollaborativeFilteringRecommendationWebsiteAgent() extends WebsiteAgent {
+  def id = "colab_filter"
+  def modifyPage(page: Page, visitor: User): Page = ???
+  def notifyUserAction(user: User, currentPage: Option[Page], action: Action): Unit = ???
+}
+
+case class MostFrequentRecommendationWebsiteAgent() extends WebsiteAgent {
+  def id = "most_freq"
+  def modifyPage(page: Page, visitor: User): Page = ???
+  def notifyUserAction(user: User, currentPage: Option[Page], action: Action): Unit = ???
 }
